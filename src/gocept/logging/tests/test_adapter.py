4d7143785434d3edf2d83591eb7d269c2a93660c
Wolfgang Schnerring <ws@gocept.com>
2013-08-24 13:51:48 +0200

    Add test to get to 100% coverage

--- src/gocept/logging/tests/test_adapter.py
+++ src/gocept/logging/tests/test_adapter.py
@@ -17,6 +17,12 @@ class StaticDefaultsTest(unittest.TestCase):
 
     def test_adds_extra_values(self):
         log = StaticDefaults(self.log, {'qux': 'baz'})
+        log.warning('')
+        self.assertEqual(
+            {'qux': 'baz'}, self.handler.messages[0].extra)
+
+    def test_adds_extra_values_to_the_one_from_logger(self):
+        log = StaticDefaults(self.log, {'qux': 'baz'})
         log.warning('', extra={'foo': 'bar'})
         self.assertEqual(
             {'foo': 'bar', 'qux': 'baz'}, self.handler.messages[0].extra)
