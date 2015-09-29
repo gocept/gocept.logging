17f8734e67e8ad9c3ab538e5f919d4985cd9aa45
Michael Howitz <mh@gocept.com>
2015-09-29 17:39:09 +0200

    Declare Python 3.5 compatibility.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013 gocept gmbh & co. kg
+# Copyright (c) 2013-2015 gocept gmbh & co. kg
 # See also LICENSE.txt
 
 # This should be only one line. If it must be multi-line, indent the second
@@ -49,6 +49,7 @@ Programming Language :: Python :: 2.7
 Programming Language :: Python :: 3
 Programming Language :: Python :: 3.3
 Programming Language :: Python :: 3.4
+Programming Language :: Python :: 3.5
 """[:-1].split('\n'),
     description=__doc__.strip(),
     long_description='\n\n'.join(open(project_path(name)).read() for name in (
