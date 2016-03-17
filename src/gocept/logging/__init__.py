09b4c1ac92e3b279a37b011c3343c671e93e86de
Steffen Allner <sa@gocept.com>
2016-03-17 14:50:53 +0100

    PEP8 fixes.

--- src/gocept/logging/__init__.py
+++ src/gocept/logging/__init__.py
@@ -1,3 +1,3 @@
-from gocept.logging.formatter import KeyValueFormatter, SyslogKeyValueFormatter
-from gocept.logging.testing import TestingHandler
-from gocept.logging.argumentparser import ArgumentParser
+from gocept.logging.formatter import KeyValueFormatter, SyslogKeyValueFormatter  # NOQA
+from gocept.logging.testing import TestingHandler  # NOQA
+from gocept.logging.argumentparser import ArgumentParser  # NOQA
